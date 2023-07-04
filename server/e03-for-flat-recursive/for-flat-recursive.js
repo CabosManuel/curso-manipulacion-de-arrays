3e0a4d7774b94e28ceccac58a7050985883be17d
CabosManuel <cabos.apolaya@gmail.com>
2023-07-04 19:16:00 -0400

    E03 for flat recursive

---
+++ server/e03-for-flat-recursive/for-flat-recursive.js
@@ -0,0 +1,14 @@
+// Flat matrix con for y recursividad 🤔
+
+const matrix = [
+	[1,2,3],
+	[4,5,6, [12,13, [21,22]]],
+	[7,8,9],
+];
+
+
+let newFlatMatrix = [];
+for (let i = 0; i < matrix.length; i++) {
+	const subMatrix = matrix[i];
+	
+}
