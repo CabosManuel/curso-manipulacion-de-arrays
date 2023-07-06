20ae4eb95214144c18fb2981955b91562467906f
CabosManuel <cabos.apolaya@gmail.com>
2023-07-06 15:16:27 -0500

    End E03 for flat recursive

--- server/e03-for-flat-recursive/for-flat-recursive.js
+++ server/e03-for-flat-recursive/for-flat-recursive.js
@@ -2,13 +2,31 @@
 
 const matrix = [
 	[1,2,3],
-	[4,5,6, [12,13, [21,22]]],
+	[4,5,6, [12,13, [1,2,3,[4]]]],
 	[7,8,9],
 ];
 
-
-let newFlatMatrix = [];
+let newArray = [];
 for (let i = 0; i < matrix.length; i++) {
-	const subMatrix = matrix[i];
-	
+	const element = matrix[i];
+	for (let j = 0; j < element.length; j++) {
+		const subElement = element[j];
+		newArray.push(subElement);
+	}
 }
+console.log(newArray);
+
+function recursive (array) {
+	let newArray = [];
+
+	if (!Array.isArray(array)) return array;
+
+	for (let i = 0; i < array.length; i++) {
+		const element = array[i];
+		newArray = newArray.concat(recursive(element));
+	}
+	
+	return newArray;
+}
+
+console.log(recursive(matrix));
